624140239b9e806f15e18a569d2bc7614250e25b
TheBud4 <murilopmr13@hotmail.com>
2022-09-23 16:21:58 -0300

    lets go

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import  express  from "express"
+const app = express()
+
+app.get('/user', (req,res)=>{
+
+})
+app.post('/user',(req,res)=>{
+
+})
+app.post('/bikes',(req,res)=>{
+
+})
+app.get('/bikes',(req,res)=>{
+
+})
+app.get('/admin',(req,res)=>{
+
+})
+app.post('/admin',(req,res)=>{
+  
+})
+app.listen(3333)
