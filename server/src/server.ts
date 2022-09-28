1924806a041e5b6c23074ac2ccef7d3694a3ceb0
TheBud4 <murilopmr13@hotmail.com>
2022-09-28 17:41:57 -0300

    tried to do a login validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,67 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, usuario } from "@prisma/client"
 import  express  from "express"
+import { body,validationResult } from "express-validator"
+import { IsEmailOptions } from "express-validator/src/options"
+import { Sql } from "@prisma/client/runtime"
 
 const app = express()
 app.use(express.json())
-const prisma = new PrismaClient({
-    log:['query']
-  })
+const prisma = new PrismaClient()
 
 // USUARIO
-app.get('/user', async (req,res)=>{
+app.get('/user',[
+
+
+], async (req: express.Request,res:express.Response)=>{
     const users = await prisma.usuario.findMany({
         select:{
             id:true,
             nome:true,
             CPF:true,
-            RG:true,
             email:true,
             senha:true
         }
     })
     return res.json(users)
 })
-app.post('/user',(req,res)=>{
-    
-})
+app.post('/user',[
+    body("email").isEmail().withMessage("O e-mail precisa ser válido"),
+    body("email").custom(value =>{
+        if(!value){
+            return Promise.reject("E-mail é obrigatório")
+        }
+
+    }),
+    body('senha').isLength({ min: 8 }).withMessage("A senha deve conter ao menos 8 caracteres"),
+    body('confirmacaosenha').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('As senhas não coincidem');
+        }
+        return true;
+      })
+
+],async(req: express.Request,res:express.Response) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const body = req.body
+    const usuario = await prisma.usuario.create({
+        data:{
+        nome: body.nome,
+        senha:body.senha,
+        CPF:body.CPF,
+        email:body.email
+        }
+      })
+      return res.status(201).json(usuario)
+  });
 app.post('/user/bikes', async(req,res)=>{
     const users = await prisma.usuario.findMany({
         select:{
             id:true,
             nome:true,
             CPF:true,
-            RG:true,
             email:true,
             senha:true
         }
@@ -71,4 +102,6 @@ app.get('/admin',(req,res)=>{
 app.post('/admin',(req,res)=>{
   
 })
-app.listen(3333)
+app.listen(3333,() =>{
+    console.log('Server ta on na porta 3333');
+})
